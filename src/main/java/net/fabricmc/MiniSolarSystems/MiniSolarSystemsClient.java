a331ba9882329d86b4eb854b86ddfc4a547be69a
Ellis Verhoest <ellis@lupimedia.com>
2021-12-24 22:00:34 +0000

    naughty gitdoall

--- src/main/java/net/fabricmc/MiniSolarSystems/MiniSolarSystemsClient.java
+++ src/main/java/net/fabricmc/MiniSolarSystems/MiniSolarSystemsClient.java
@@ -3,12 +3,22 @@ package net.fabricmc.MiniSolarSystems;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.model.ModelLoadingRegistry;
 import net.fabricmc.fabric.api.client.rendering.v1.BlockEntityRendererRegistry;
+import net.minecraft.client.render.block.entity.BlockEntityRenderer;
+import net.minecraft.client.render.block.entity.BlockEntityRendererFactory;
 
 public class MiniSolarSystemsClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         ModelLoadingRegistry.INSTANCE.registerResourceProvider(rm -> new ModelProvider());
 
-        BlockEntityRendererRegistry.register(MiniSolarSystems.starBlockEntity, StarBlockEntityRenderer::new);
+        BlockEntityRendererRegistry.register(
+                MiniSolarSystems.starBlockEntity,
+                new BlockEntityRendererFactory<StarBlockEntity>() {
+                    @Override
+                    public BlockEntityRenderer<StarBlockEntity> create(Context ctx) {
+                        return new StarBlockEntityRenderer();
+                    }
+                }
+        );
     }
 }
--- src/main/java/net/fabricmc/MiniSolarSystems/StarBlockEntityRenderer.java
+++ src/main/java/net/fabricmc/MiniSolarSystems/StarBlockEntityRenderer.java
@@ -1,17 +1,32 @@
 package net.fabricmc.MiniSolarSystems;
 
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.block.entity.BlockEntityRenderDispatcher;
 import net.minecraft.client.render.block.entity.BlockEntityRenderer;
+import net.minecraft.client.render.model.json.ModelTransformation;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
+import net.minecraft.util.math.Vec3f;
 
 public class StarBlockEntityRenderer implements BlockEntityRenderer<StarBlockEntity> {
     private static ItemStack stack = new ItemStack(Items.DIRT, 1);
 
     @Override
     public void render(StarBlockEntity blockEntity, float tickDelta, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, int overlay) {
+        matrices.push();
+        // Calculate the current offset in the y value
+        double offset = Math.sin((blockEntity.getWorld().getTime() + tickDelta) / 8.0) / 4.0;
+        // Move the item
+        matrices.translate(0.5, 1.25 + offset, 0.5);
 
+        // Rotate the item
+        matrices.multiply(Vec3f.POSITIVE_Y.getDegreesQuaternion((blockEntity.getWorld().getTime() + tickDelta) * 4));
+
+        MinecraftClient.getInstance().getItemRenderer().renderItem(stack, ModelTransformation.Mode.GROUND, light, overlay, matrices, vertexConsumers, 0);
+
+        // Mandatory call after GL calls
+        matrices.pop();
     }
 }
